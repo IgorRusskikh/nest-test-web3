507a2d056f41907a0b66e523eb2e39d8d16eb675
russkikh_igor <russkikh2.igor3.2003@gmail.com>
2025-07-07 22:35:37 +0700

    feat: define base evm data interfaces

---
+++ src/app/types/evm.types.ts
@@ -0,0 +1,50 @@
+export interface EvmBlock {
+  number: string;
+  hash: string;
+  parentHash: string;
+  gasLimit: string;
+  gasUsed: string;
+  size: string;
+  timestamp: string;
+  transactions: string[];
+  [key: string]: any;
+}
+
+export interface EvmTransaction {
+  hash: string;
+  to: string | null;
+  from: string;
+  value: string;
+  input: string;
+  maxFeePerGas?: string;
+  maxPriorityFeePerGas?: string;
+  gasPrice?: string;
+  [key: string]: any;
+}
+
+export interface EvmBlockResponse {
+  height: number;
+  hash: string;
+  parentHash: string;
+  gasLimit: string;
+  gasUsed: string;
+  size: string;
+}
+
+export interface EvmTransactionResponse {
+  hash: string;
+  to: string | null;
+  from: string;
+  value: string;
+  input: string;
+  maxFeePerGas?: string;
+  maxPriorityFeePerGas?: string;
+  gasPrice?: string;
+}
+
+export type EvmRpcPayload = {
+  jsonrpc: string;
+  method: string;
+  params: (string | number | boolean)[];
+  id: number;
+};
